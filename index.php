78855d721a0259d9068586bf5600802c13125668
PazitoPazos <javier.pazos001@gmail.com>
2022-01-13 12:50:19 +0100

    Operadores aritméticos

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h3>Construción de expresións en PHP</h3>
+
+        <?php
+        // Operadores aritméticos
+        $x=10;
+        $y=3;
+        $z=$x*$y;
+        echo $x." x ".$y." = ".$z."<br/>";
+        $z=$x%$y;
+        echo $x." módulo ".$y." = ".$z."<br/>";
+        ?>
+    </body>
+</html>
